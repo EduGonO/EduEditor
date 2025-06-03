b604fd51430c3499127ac4bd35edbc4d0c1861eb
edu gonzalez <320ego@gmail.com>
2025-06-03 13:16:14 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   ],
   targets: [
     .target(
-      name: "EduEditor",
+      name: "EduEditorSwift",
       resources: [
         .process("Resources/EditorBundle")
       ]
