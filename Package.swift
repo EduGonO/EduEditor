f985a84df16c7c3eeef8dc8f2c36838848157da7
edu gonzalez <46654892+EduGonO@users.noreply.github.com>
2025-06-03 13:33:48 +0200

    package swift 5.9

--- Package.swift
+++ Package.swift
@@ -1,20 +1,22 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
-  name: "EduEditor",
-  platforms: [
-    .iOS(.v15), .macOS(.v12)
-  ],
-  products: [
-    .library(name: "EduEditor", targets: ["EduEditor"]),
-  ],
-  targets: [
-    .target(
-      name: "EduEditor",
-      resources: [
-        .process("Resources/EditorBundle")
-      ]
-    )
-  ]
+    name: "EduEditor",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
+    products: [
+        .library(name: "EduEditor", targets: ["EduEditor"])
+    ],
+    targets: [
+        .target(
+            name: "EduEditor",
+            path: "Sources/EduEditor",
+            resources: [
+                .process("../../Resources/EditorBundle")
+            ]
+        )
+    ]
 )
